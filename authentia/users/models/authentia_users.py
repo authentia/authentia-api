1e98930903e1cf022ee7746bafbacc6038e1e995
svmalvaez <svmalvaez@gmail.com>
2018-04-28 12:36:36 -0500

    Photo enrolled model

--- authentia/users/models/authentia_users.py
+++ authentia/users/models/authentia_users.py
@@ -2,6 +2,8 @@ from django.contrib.gis.db import models
 from django.core import signing
 
 from users.models import AuthentiaAbstractBaseUser, TokenUserModel
+from base.models import BaseModel
+from helpers import upload_to_kwargs, upload_to_generic
 
 
 class AuthentiaUser(AuthentiaAbstractBaseUser):
@@ -12,3 +14,15 @@ class AuthentiaUser(AuthentiaAbstractBaseUser):
 
     def __str__(self):
         return "{} {} {}".format(self.pk, self.full_name, self.token)
+
+
+class PhotoEnroll(BaseModel):
+    user = models.ForeignKey(AuthentiaUser, on_delete=models.CASCADE)
+    file = models.ImageField(upload_to=upload_to_kwargs(upload_to_generic, subfolder="photos-enrolled"))
+
+    # def save(self):
+    #     enroll with api
+    #     pass
+
+    def __str__(self):
+        return self.user.first_name
