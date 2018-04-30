a070287a5a73b0a6b382bcece73e28bd95cffd5a
svmalvaez <svmalvaez@gmail.com>
2018-04-30 15:43:08 -0500

    Enroll photo url

--- authentia/api/serializers/photos_enrolled.py
+++ authentia/api/serializers/photos_enrolled.py
@@ -13,7 +13,7 @@ class PhotoEnrollSerializer(serializers.ModelSerializer):
         user = self.context.get('request').user
         document = PhotoEnroll.objects.create(user=user, file=validated_data.get('file'), created=validated_data)
         # response = user.enroll_user(self.context.get('request').build_absolute_uri(document.file.url))
-        response = user.enroll_user(document.file.url)
+        response = user.enroll_user(document.file)
         if response.get('Errors'):
             document.delete()
             raise serializers.ValidationError(response.get('Errors')[0].get('Message'))
