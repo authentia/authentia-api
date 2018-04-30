7218aa94cba90c06e0ff05f65f7b57b0513cbef2
svmalvaez <svmalvaez@gmail.com>
2018-04-30 16:17:23 -0500

    Fix transaction admin

--- authentia/companies/models/transactions.py
+++ authentia/companies/models/transactions.py
@@ -16,4 +16,4 @@ class Transaction(BaseModel):
     photo = models.ImageField(upload_to=upload_to_kwargs(upload_to_generic, subfolder="transaction-photo"))
 
     def __str__(self):
-        return "{} {}".format(self.company.name, self.user.first_name)
+        return self.pk
