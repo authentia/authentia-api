b1a205093c70c39c566096174b24a1b21548d1ee
svmalvaez <svmalvaez@gmail.com>
2018-04-30 15:40:53 -0500

    Update photo url

--- authentia/api/serializers/photos_enrolled.py
+++ authentia/api/serializers/photos_enrolled.py
@@ -12,8 +12,8 @@ class PhotoEnrollSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = self.context.get('request').user
         document = PhotoEnroll.objects.create(user=user, file=validated_data.get('file'), created=validated_data)
-        response = user.enroll_user(self.context.get('request').build_absolute_uri(document.file.url))
-        # response = user.enroll_user('http://media.kairos.com/kairos-elizabeth.jpg')
+        # response = user.enroll_user(self.context.get('request').build_absolute_uri(document.file.url))
+        response = user.enroll_user(document.file.url)
         if response.get('Errors'):
             document.delete()
             raise serializers.ValidationError(response.get('Errors')[0].get('Message'))
