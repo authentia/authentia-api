1b94027c125eba02469ce3acfecb74153060203d
svmalvaez <svmalvaez@gmail.com>
2018-04-30 15:21:36 -0500

    Add body to Lead

--- authentia/users/models/leads.py
+++ authentia/users/models/leads.py
@@ -6,6 +6,7 @@ from base.models import BaseModel
 class Lead(BaseModel):
     name = models.CharField(max_length=64)
     email = models.EmailField(max_length=64)
+    body = models.TextField(default='')
 
     def __str__(self):
         return "{} {} {}".format(self.pk, self.name, self.email)
